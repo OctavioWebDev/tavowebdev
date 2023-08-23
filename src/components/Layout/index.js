203ad5af3468dcb8aab9ec27608d7da8233e2e5b
babotavo419 <one.faith419@gmail.com>
2023-08-22 21:26:53 -0400

    started navbar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,12 @@
+import Sidebar from '../Sidebar'
 import './index.scss';
 
 const Layout = () => {
-    return <>hello</>
+    return (
+        <>
+            < Sidebar />
+        </>
+    )
 }
 
 export default Layout;
