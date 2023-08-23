d3a5da3b1d77c6bf6deacdabd4de910f118359e1
babotavo419 <one.faith419@gmail.com>
2023-08-23 05:23:19 -0400

    ok

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
 import './index.scss';
-
+import LogoO from '../../assets/images/logo-o.png'
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
+            <img src={LogoO} alt="logo" />
+            <img src={LogoO} alt="logo" />
         </Link>
     </div>
 )
