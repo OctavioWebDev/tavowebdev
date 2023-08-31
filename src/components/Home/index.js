e4ce6de637460ab402906883042ed3532f10c09d
babotavo419 <one.faith419@gmail.com>
2023-08-31 06:12:21 -0400

    must fix email

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import LogoTitle from '../../assets/images/logo_o.png'
 import { Link } from 'react-router-dom';
+import Loader from 'react-loaders'
 import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
@@ -15,35 +16,41 @@ const Home = () => {
         const timer = setTimeout(() => {
             setLetterClass('text-animate-hover');
         }, 4000);
-    
+        
         return () => clearTimeout(timer);
     }, []);
     
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i</span>
-                    <br/>
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt="developer"/>
-                <AnimatedLetters letterClass={letterClass} 
-                strArray={nameArray} 
-                idx={15}/>
-                <br />
-                <AnimatedLetters letterClass={letterClass} 
-                strArray={jobArray} 
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i</span>
+                        <br/>
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt="developer"/>
+                    <AnimatedLetters 
+                        letterClass={letterClass} 
+                        strArray={nameArray} 
+                        idx={15}
+                    />
+                    <br />
+                    <AnimatedLetters 
+                        letterClass={letterClass} 
+                        strArray={jobArray} 
                 idx={22}/>
                 </h1>
-                <h2>Fullstack Developer / JavaScript</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                    <h2>Fullstack Developer / JavaScript</h2>
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+                <Logo/>
             </div>
-            <Logo/>
-        </div>
+            <Loader type="pacman" />
+        </>
     );
-
 }
-
-export default Home
+    
+export default Home;
+    
