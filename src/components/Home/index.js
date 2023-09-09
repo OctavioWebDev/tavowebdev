16a9e5536eeb56c5e9ced0b1078a1df47e17d6e3
babotavo419 <one.faith419@gmail.com>
2023-09-09 19:05:22 -0400

    ok

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import Loader from 'react-loaders'
 import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import bubblePopSound from '../../assets/soap-bubbles-pop-96873.mp3';
 
 
 
@@ -11,6 +12,8 @@ const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['c', 't', 'a', 'v', 'i', 'o',',']
     const jobArray = ['A',' ','w', 'e', 'b',' ','d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+    const audio = new Audio(bubblePopSound);
+
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -19,29 +22,34 @@ const Home = () => {
         
         return () => clearTimeout(timer);
     }, []);
+
+    const handleHover = () => {
+        audio.play();
+    };
     
     return (
         <>
             <div className="container home-page">
                 <div className="text-zone">
                     <h1>
-                        <span className={letterClass}>H</span>
-                        <span className={`${letterClass} _12`}>i</span>
+                        <span className={letterClass} onMouseEnter={handleHover}>H</span>
+                        <span className={`${letterClass} _12`} onMouseEnter={handleHover}>i</span>
                         <br/>
-                        <span className={`${letterClass} _13`}>I</span>
-                        <span className={`${letterClass} _14`}>'m</span>
+                        <span className={`${letterClass} _13`} onMouseEnter={handleHover}>I</span>
+                        <span className={`${letterClass} _14`} onMouseEnter={handleHover}>'m</span>
                     <img src={LogoTitle} alt="developer"/>
                     <AnimatedLetters 
                         letterClass={letterClass} 
                         strArray={nameArray} 
                         idx={15}
-                    />
+                        />
                     <br />
                     <AnimatedLetters 
                         letterClass={letterClass} 
                         strArray={jobArray} 
-                idx={22}/>
-                </h1>
+                        idx={22}
+                        />
+                    </h1>
                     <h2>Fullstack Developer / JavaScript</h2>
                     <Link to="/contact" className="flat-button">CONTACT ME</Link>
                 </div>
